77a0156532d4aeac3c940c89a21267e8acba1ed6
Jeff Ober <jober@papa.com>
2023-07-06 09:16:26 -0600

    Improve validation, make it easier to clear the form input

--- lib/words_etc_web/components/word_solver_form.ex
+++ lib/words_etc_web/components/word_solver_form.ex
@@ -15,21 +15,31 @@ defmodule WordsEtcWeb.Components.WordSolverForm do
       >
         <input type="hidden" name="_csrf_token" value={Plug.CSRFProtection.get_csrf_token()} />
 
-        <div class="col-8">
+        <div class="col-6 position-relative">
           <label for="letters" class="form-label">Letters</label>
+
           <input
             id="letters"
             type="text"
             name="letters"
-            class="form-control"
+            class="form-control text-uppercase"
             required
-            pattern="[a-zA-Z]*"
+            pattern="[A-Z?]*"
             value={@letters}
             autocomplete="off"
             autocorrect="off"
             autocapitalize="off"
           />
-          <div class="invalid-feedback">Please enter letters only.</div>
+
+          <button
+            id="clear-letters"
+            type="button"
+            class="btn btn-primary rounded-pill position-absolute top-50 end-0 translate-middle-y"
+          >
+            X
+          </button>
+
+          <div class="invalid-feedback">Please enter letters or ? only.</div>
         </div>
 
         <div class="col-4 d-flex align-items-end">
@@ -38,17 +48,27 @@ defmodule WordsEtcWeb.Components.WordSolverForm do
       </form>
     </div>
 
-    <script>
+    <script type="text/javascript">
       $(document).ready(function() {
-        // Disable the submit button after click
-        $("#submit-button").on('click', function() {
-          $(this).prop('disabled', true);
-          return true;
+        // Clear form input when X is clicked
+        $("#clear-letters").on('click', function() {
+          $("#letters").val('');
+          $("#letters").focus();
+        });
+
+        // When form input is clicked, select all text in the input
+        $("#letters").on('click', function() {
+          $(this).select();
+        });
+
+        $("#letters").on('input', function() {
+          $(this).val($(this).val().toUpperCase());
         });
 
         // Bootstrap form validation
         (function () {
           'use strict'
+
           // Fetch all the forms we want to apply custom Bootstrap validation styles to
           var forms = document.querySelectorAll('.needs-validation')
 
