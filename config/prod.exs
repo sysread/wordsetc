2d15a6c791e1e9be77f7a91c4f896ca0e2672487
Jeff Ober <sysread@fastmail.fm>
2023-07-26 13:21:49 -0600

    disambiguate pwa manifest.json and phoenix manifest.json

--- config/prod.exs
+++ config/prod.exs
@@ -10,5 +10,5 @@ config :words_etc, WordsEtcWeb.Endpoint,
   http: [:inet6, port: 4000],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   server: true,
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
