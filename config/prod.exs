595dc814f864912d94dd30317776a81a62d48bbf
Jeff Ober <sysread@fastmail.fm>
2023-07-26 13:08:32 -0600

    Add prod config for http

--- config/prod.exs
+++ config/prod.exs
@@ -5,3 +5,10 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+config :words_etc, WordsEtcWeb.Endpoint,
+  url: [host: "wordsetc.tplinkdns.com", scheme: "https", port: 443],
+  http: [:inet6, port: 4000],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  server: true,
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
