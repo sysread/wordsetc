70e1cbb83b9f77123dea234108ac70618e6a809a
Jeff Ober <sysread@fastmail.fm>
2023-07-26 14:27:44 -0600

    Fix inconsistent http vs https configs

--- config/prod.exs
+++ config/prod.exs
@@ -5,7 +5,3 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
-config :words_etc, WordsEtcWeb.Endpoint,
-  url: [host: "wordsetc.tplinkdns.com", scheme: "https", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
