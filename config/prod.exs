1edf156a24298373727e59ac2da337ed56589154
Jeff Ober <sysread@fastmail.fm>
2023-07-26 12:32:28 -0600

    Change port to use expected forwarding port

--- config/prod.exs
+++ config/prod.exs
@@ -5,18 +5,17 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
-
 config :words_etc, WordsEtcWeb.Endpoint,
-  http: [:inet6, port: {:system, "PORT"}],
+  http: [:inet6, port: 4000],
   url: [host: "wordsetc.tplinkdns.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   server: true,
   https: [
     :inet6,
-    port: 443,
+    port: 4000,
     cipher_suite: :strong,
     certfile: "/etc/letsencrypt/live/wordsetc.tplinkdns.com/fullchain.pem",
     keyfile: "/etc/letsencrypt/live/wordsetc.tplinkdns.com/privkey.pem",
-    transport_options: [socket_opts: [:reuseaddr]]
+    transport_options: [socket_opts: [:reuseaddr, :inet6]]
   ]
