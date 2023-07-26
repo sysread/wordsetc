1366c68ef8c08c70eecea58693682b08cb1561f0
Jeff Ober <sysread@fastmail.fm>
2023-07-26 13:32:06 -0600

    I do no think I need all of this prod config...

--- config/prod.exs
+++ config/prod.exs
@@ -7,8 +7,5 @@ config :logger, level: :info
 # of environment variables, is done on config/runtime.exs.
 config :words_etc, WordsEtcWeb.Endpoint,
   url: [host: "wordsetc.tplinkdns.com", scheme: "https", port: 443],
-  http: [:inet6, port: 4000],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  server: true,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+  cache_static_manifest: "priv/static/cache_manifest.json"
